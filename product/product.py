d47335075918ad903b1852906acde737872f4702
JanRockIT <programmieren001@gmail.com>
2025-04-02 21:40:28 +0200

    wrong version with uuid direct in python

--- product/product.py
+++ product/product.py
@@ -8,14 +8,31 @@ print("Welcome to the product from *DAX_knoel*")
 
 time.sleep(2)
 
-if input("Do you want to register the product? (Y/n): ").lower not in ["", " ", "  ", "   ", "    ", "y", "ye", "yes", "ys", "yse", "sye"]:
+if input("Do you want to register the product? (Y/n): ").lower() not in ["", " ", "  ", "   ", "    ", "y", "ye", "yes", "ys", "yse", "sye"]:
     sys.exit("The product is still unused.")
 
-uuid: str = generate_uuid()
-ip: str = get_ip()
+uuid = generate_uuid()
+ip = get_ip()
 
 if ip.startswith("Error"):
     sys.exit(ip)
 
-# request...
-# logged in...
+check_pair_url = "https://license-file.onrender.com/check-pair"
+send_pair_url = "https://license-file.onrender.com/send-pair"
+
+payload = {
+    "UUID": uuid,
+    "ipAddress": ip
+}
+
+check_pair_response = requests.post(check_pair_url, json=payload)
+
+if check_pair_response.status_code != 200:
+    sys.exit(check_pair_response)
+
+send_pair_response = requests.post(send_pair_url, payload)
+
+if send_pair_response.status_code != 201:
+    sys.exit(send_pair_response)
+
+print("Successfully registered!")
